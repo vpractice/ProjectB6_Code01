dcddaa297cc28d61faae8b45e47c42e932e2ab35
vpractice <68060284+vpractice@users.noreply.github.com>
2022-02-12 23:06:01 +0500

    updated file for new code

--- newfeature.sh
+++ newfeature.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Iam writing code in dev branch"
-
+echo "Iam writing code in dev branch"echo "Iam writing code in dev branch"echo "Iam writing code in dev branch"
 echo "end of line"
